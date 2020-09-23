c3d59ba0b211767a897b29e94853e512b6f57b06
MacacoDev <71792319+Macaco-athenas@users.noreply.github.com>
2020-09-23 16:45:29 -0400

    Add files via upload

---
+++ LauncherLA.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LauncherLA
+{
+    partial class LauncherLA
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LauncherLA));
+            this.Play = new System.Windows.Forms.Button();
+            this.Fechar = new System.Windows.Forms.Button();
+            this.Instagram = new System.Windows.Forms.Button();
+            this.Facebook = new System.Windows.Forms.Button();
+            this.Discord = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Transparent;
+            this.Play.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Play.BackgroundImage")));
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Play.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Play.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Play.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.ForeColor = System.Drawing.Color.White;
+            this.Play.Location = new System.Drawing.Point(289, 369);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(139, 43);
+            this.Play.TabIndex = 0;
+            this.Play.UseMnemonic = false;
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Fechar
+            // 
+            this.Fechar.BackColor = System.Drawing.Color.Transparent;
+            this.Fechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Fechar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Fechar.FlatAppearance.BorderSize = 0;
+            this.Fechar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Fechar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Fechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Fechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fechar.ForeColor = System.Drawing.SystemColors.Control;
+            this.Fechar.Location = new System.Drawing.Point(678, 12);
+            this.Fechar.Name = "Fechar";
+            this.Fechar.Size = new System.Drawing.Size(30, 30);
+            this.Fechar.TabIndex = 2;
+            this.Fechar.Text = "X";
+            this.Fechar.UseVisualStyleBackColor = false;
+            this.Fechar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Instagram
+            // 
+            this.Instagram.BackColor = System.Drawing.Color.Transparent;
+            this.Instagram.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Instagram.BackgroundImage")));
+            this.Instagram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Instagram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Instagram.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Instagram.FlatAppearance.BorderSize = 0;
+            this.Instagram.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Instagram.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Instagram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Instagram.Location = new System.Drawing.Point(12, 12);
+            this.Instagram.Name = "Instagram";
+            this.Instagram.Size = new System.Drawing.Size(35, 35);
+            this.Instagram.TabIndex = 3;
+            this.Instagram.UseVisualStyleBackColor = false;
+            this.Instagram.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Facebook
+            // 
+            this.Facebook.BackColor = System.Drawing.Color.Transparent;
+            this.Facebook.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Facebook.BackgroundImage")));
+            this.Facebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Facebook.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Facebook.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Facebook.FlatAppearance.BorderSize = 0;
+            this.Facebook.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Facebook.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Facebook.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Facebook.Location = new System.Drawing.Point(12, 56);
+            this.Facebook.Name = "Facebook";
+            this.Facebook.Size = new System.Drawing.Size(35, 35);
+            this.Facebook.TabIndex = 4;
+            this.Facebook.UseVisualStyleBackColor = false;
+            this.Facebook.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Discord
+            // 
+            this.Discord.BackColor = System.Drawing.Color.Transparent;
+            this.Discord.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Discord.BackgroundImage")));
+            this.Discord.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Discord.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Discord.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Discord.FlatAppearance.BorderSize = 0;
+            this.Discord.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Discord.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Discord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Discord.Location = new System.Drawing.Point(12, 100);
+            this.Discord.Name = "Discord";
+            this.Discord.Size = new System.Drawing.Size(35, 35);
+            this.Discord.TabIndex = 5;
+            this.Discord.UseVisualStyleBackColor = false;
+            this.Discord.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(644, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(553, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "© AthenasRP All rights reserved.";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Play);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 482);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // LauncherLA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(720, 480);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Discord);
+            this.Controls.Add(this.Facebook);
+            this.Controls.Add(this.Instagram);
+            this.Controls.Add(this.Fechar);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 480);
+            this.MinimumSize = new System.Drawing.Size(720, 480);
+            this.Name = "LauncherLA";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LauncherLA";
+            this.Load += new System.EventHandler(this.LauncherLA_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Fechar;
+        private System.Windows.Forms.Button Instagram;
+        private System.Windows.Forms.Button Facebook;
+        private System.Windows.Forms.Button Discord;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LauncherLA.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LauncherLA
+{
+    public partial class LauncherLA : Form
+    {
+        int mov;
+        int movX;
+        int movY;
+        public LauncherLA()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("ts3client_win64");
+            if (processes.Length <= 0)
+            {
+                System.Diagnostics.Process.Start($"fivem://connect/athenasrp.cf"); System.Diagnostics.Process.Start($"ts3server://athenasrp.meuts3.com");
+                this.Close();
+            }
+            else
+            {
+                System.Diagnostics.Process.Start($"fivem://connect/athenasrp.cf");
+                this.Close();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"https://www.instagram.com/_losangelesrp_/");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"https://bit.ly/2Lk4vDP");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"https://discord.gg/MZhWBQa");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void LauncherLA_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+        
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }   
+}
